ffa43f3a206fc9a3c244fe7a316217e4558ff766
Luka Managadze <managaluka0@gmail.com>
2025-07-28 21:57:31 +0400

    Initial Commit

---
+++ MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by Luka Managadze on 7/28/25.
+//
+
+import Testing
+@testable import Moonshot
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
