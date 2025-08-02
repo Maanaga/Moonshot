832fe4b30a52fc2ea60755296644da399e6cbf70
Luka Managadze <managaluka0@gmail.com>
2025-08-02 20:04:22 +0400

    Initial Commit

--- Moonshot/CrewScrollView.swift
+++ Moonshot/CrewScrollView.swift
@@ -1,8 +1,47 @@
-//
-//  CrewScrollView.swift
-//  Moonshot
-//
-//  Created by Luka Managadze on 8/2/25.
-//
 
-import Foundation
+import SwiftUI
+
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
+
+struct CrewScrollView: View {
+    let mission: Mission
+    let crew: [CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(.circle)
+                                .shadow(radius: 10)
+                                .overlay(
+                                    Circle()
+                                        .strokeBorder(.white, lineWidth: 0.6)
+                                )
+                            
+                            
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                
+                                Text(crewMember.role)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -2,10 +2,7 @@
 import SwiftUI
 
 struct MissionView: View {
-    struct CrewMember {
-        let role: String
-        let astronaut: Astronaut
-    }
+   
     let mission: Mission
     let crew: [CrewMember]
     var body: some View {
@@ -50,39 +47,7 @@ struct MissionView: View {
                         .padding(.bottom, 5)
                 }
                 .padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(crew, id: \.role) { crewMember in
-                            NavigationLink {
-                                AstronautView(astronaut: crewMember.astronaut)
-                            } label: {
-                                HStack {
-                                    Image(crewMember.astronaut.id)
-                                        .resizable()
-                                        .frame(width: 104, height: 72)
-                                        .clipShape(.circle)
-                                        .shadow(radius: 10)
-                                        .overlay(
-                                            Circle()
-                                                .strokeBorder(.white, lineWidth: 0.6)
-                                        )
-                                    
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text(crewMember.astronaut.name)
-                                            .foregroundStyle(.white)
-                                            .font(.headline)
-                                        
-                                        Text(crewMember.role)
-                                            .foregroundStyle(.white.opacity(0.5))
-                                        }
-                                    }
-                                .padding(.horizontal)
-                                }
-                            }
-                        }
-                }
+                CrewScrollView(mission: mission, crew: crew)
             }
             .padding(.bottom)
         }
@@ -103,6 +68,7 @@ struct MissionView: View {
         }
     }
 }
+
 
 #Preview {
     let missions: [Mission] = Bundle.main.decode("missions.json")
